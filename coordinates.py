d1ab8465f2e4dc6c1099dac360ec55f29229d961
Wesley Chou <wcia013twe>
2023-10-07 14:01:08 -0400

    import firebase

--- coordinates.py
+++ coordinates.py
@@ -1,2 +1,20 @@
-print("Test")
+from firebase_admin import db
 
+ref = db.reference('https://refill-radar-database-default-rtdb.firebaseio.com/')
+
+class Retrieve:
+    def _init_(self, location, longitude, latitude, image, direction):
+        self.location = location
+        self.longitude = longitude
+        self.latitude = latitude
+        self.image = image
+        self.direction = direction
+
+    def _repr_(self):
+        return f"Location(\
+                location={self.location}, \
+                longitude={self.longitude}, \
+                latitude={self.latitude}, \
+                image={self.image}, \
+                direction={self.direction}\
+            )"
